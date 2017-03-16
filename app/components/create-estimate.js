525348e058fe3db810c2461236dad1e1b93f9b98
Yulia Pilipenko <ypilipenko@itera-research.com>
2017-03-16 18:30:06 +0200

    .

--- app/components/create-estimate.js
+++ app/components/create-estimate.js
@@ -2,19 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     newEstimate: {
-        title: null,
-        created: null,
+      title: null,
+      created: null,
+      section: null,
+      comment: null
     },
     clearEstimate: {
-        title: null,
-        created: null,
+      title: null,
+      created: null,
     },
     findedEstimate: null,
     estimate_ID: null,
     store: Ember.inject.service(),
     actions: {
       createEstimate() {
-        var creationDate = new Date();
+        var creationDate = new Date();        
         this.set('newEstimate.created', creationDate);
 
         var _store = this.get('store'),
@@ -22,17 +24,19 @@ export default Ember.Component.extend({
 
         let estimate = _store.createRecord('estimate', newEstimate);
         _store.findRecord('user', this.get('estimate_id')).then((user) => {
-          user.get('estimates').addObject(estimate);
-          // var findedEstimate = this.get('estimates');
-          // if(typeof findedEstimate[0] !== "undefined") {
-          //   this.set('findedEstimate', findedEstimate[0]);
-          //   this.set('estimate_ID', findedEstimate[0].id);
-          // }
+          user.get('estimates').addObject(estimate);          
           user.save();
           estimate.save();
         });
         this.set('newEstimate', this.get('clearEstimate'));
         console.log(newEstimate);
+        
+        // var findedEstimate = this.get('estimates');
+        // if(typeof findedEstimate[0] !== "undefined") {
+        //   this.set('findedEstimate', findedEstimate[0]);
+        //   this.set('estimate_ID', findedEstimate[0].id);
+        // }
+        // console.log(estimate_ID);
       }
     }
 });
